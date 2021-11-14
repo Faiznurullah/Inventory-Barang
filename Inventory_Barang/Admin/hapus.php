5dcaf5522e4d0c311c10e1d34599f41e2dca9f74
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2021-11-14 12:57:18 +0700

    update to alter

--- Inventory_Barang/Admin/hapus.php
+++ Inventory_Barang/Admin/hapus.php
@@ -1,6 +1,34 @@
 <?php
 include 'koneksi.php';
+
+
 $delete = mysqli_query($conn, "DELETE FROM masuk WHERE id = '".$_GET['id']."'");
+
+
+
+$query = mysqli_query($conn, "SELECT * FROM masuk ORDER BY id");
+$hasil = ($query);
+
+// nilai awal increment
+$no = 1;
+
+while ($data  = mysqli_fetch_array($hasil))
+{
+   // membaca id dari record yang tersisa dalam tabel
+   $id = $data['id'];
+
+   // proses updating id dengan nilai $no
+   $query2 = mysqli_query($conn, "UPDATE masuk SET id = $no WHERE id = $id");
+
+
+   // increment $no
+   $no++;
+}
+
+// mengubah nilai auto increment menjadi $no terakhir ditambah 1
+$query = mysqli_query($conn, "ALTER TABLE masuk  AUTO_INCREMENT = $no");
+
+
 
  if($delete){
 	header('location: Data.php');
